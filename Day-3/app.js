6ce3bb0309a3a424fb55f609cd76cba0f99b5e0e
rajrohit10 <rohit.rajsurya10@gmail.com>
2021-08-11 20:06:37 +0530

    adding lecture-3

---
+++ Day-3/app.js
@@ -0,0 +1,33 @@
+let x;
+x=21;
+console.log(x)
+let y=21;
+y=21
+let add=x+y
+console.log(add);
+
+const student="Ayush";
+// student="Ankit"
+console.log(student)
+
+//Arrays
+let student1="Rohit";
+let student2="Ram";
+console.log(student1+student2)
+console.log("Hello There "+student1+" & "+student2)
+// Template Literals
+console.log(`Hello There ${student2} & ${student1}`)
+
+
+console.log(student1)
+console.log("Ram - Class 10")
+let studentArray=["Rohit","Ram","Aditi","Ankit","Ayush","Kushagra","Ayush","Kushagra"]
+console.log(studentArray[3]+"Class 10")
+
+//FOR LOOP,FOREACH LOOP , WHILE LOOP<DO WHILE LOOP, CONDITIONAL STATEMENTS(if,else if ,else), TERNARY OPERTORS,SWITCH,
+//FUNCTION
+//ARROW FUNCTION
+for(let i=0;i<studentArray.length;i++){
+    console.log(studentArray[i]+" FAIL/PASS")
+}
+
